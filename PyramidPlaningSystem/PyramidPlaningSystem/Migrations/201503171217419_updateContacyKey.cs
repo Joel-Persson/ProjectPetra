0dc0f69b274edf281acb5cf3b44e7e5a6ff069ba
majamalmqvist <majamalmqvist@gmail.com>
2015-03-17 15:38:11 +0100

    Lite layout ändringar i _externalLoginListParial.cshtml

---
+++ PyramidPlaningSystem/PyramidPlaningSystem/Migrations/201503171217419_updateContacyKey.cs
@@ -0,0 +1,16 @@
+namespace PyramidPlaningSystem.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class updateContacyKey : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
