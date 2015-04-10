d26ae99f4eee2f38a32a00da1173f95b334741a3
Joel Persson <persson.joel@hotmail.com>
2015-04-10 09:22:20 +0200

    Edit changes

--- PyramidPlaningSystem/PyramidPlaningSystem/Angular/Route/Route.js
+++ PyramidPlaningSystem/PyramidPlaningSystem/Angular/Route/Route.js
@@ -16,6 +16,11 @@
             controller: 'listAllToDosController',
             templateUrl: '/Angular/HtmlTemplates/toDos.html'
         })
+               .when('/editToDo',
+        {
+            controller: 'editToDoController',
+            templateUrl: '/Angular/HtmlTemplates/editToDo.html'
+        })
         .otherwise({
             redirectTo: '/'
         });
